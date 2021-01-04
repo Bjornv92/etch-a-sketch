5493376a0a3c6a8572f694ce1cd1f63dd7442f7d
Bjorn Vanhees <bjornvanhees@gmail.com>
2021-01-04 17:06:48 +0100

    Updating Grid with a working range input

--- scripts.js
+++ scripts.js
@@ -1,12 +1,42 @@
-function createSquares(){
-    for(let i = 0; i < 16; i++) {
-        let row = document.createElement("div");
-        row.className = "row";
-        for (let j = 0; j < 16; j++) {
+const mainContainer = document.getElementById("mainContainer");
+const squareDim = 960/10;
+let dimensions = document.getElementById("dimensions");
+dimensions.onchange = () => dimensions.value;
+
+function resetGrid() {
+    mainContainer.querySelectorAll("*").forEach(n => n.remove());
+}
+
+function createSquares() {
+
+    resetGrid();
+
+    for (let i = 0; i < 10; i++) {
+        let col = document.createElement("div");
+        col.className = "col";
+        for (let j = 0; j < 10; j++){
             let square = document.createElement("div");
             square.className = "square";
-            row.appendChild(square);
+            square.style.width = squareDim + "px";
+            square.style.height = squareDim + "px";
+            col.appendChild(square);
         }
-        document.getElementById("mainContainer").appendChild(row);
+        mainContainer.appendChild(col);
     }
 }
+
+/*
+
+function createSquares(){
+    for(let i = 0; i < 4; i++) {
+        let col = document.createElement("div");
+        col.className = "col";
+        for (let j = 0; j < 8; j++) {
+            let square = document.createElement("div");
+            square.className = "square";
+            col.appendChild(square);
+        }
+        document.getElementById("mainContainer").appendChild(col);
+    }
+}
+*/
